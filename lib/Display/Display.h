d997bfaed38a2c6b87aef2aec16ab17ad6d9e06d
Ben Ferdinandus <2flyfish@gmail.com>
2021-05-29 21:50:56 +0200

    moved display functionality to separate class

---
+++ lib/Display/Display.h
@@ -0,0 +1,34 @@
+//
+// Created by Ben Ferdinandus on 29-5-2021.
+//
+
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+#include <stdint-gcc.h>
+#include <Knobs.h>
+#include "U8x8lib.h"
+
+
+class Display
+{
+ private:
+	U8X8_SSD1306_128X64_NONAME_HW_I2C* u8x8;
+
+	unsigned long t = 0;
+	const int displayFps = 15;
+	const int updateDelay = 1000 / displayFps;
+
+
+ public:
+	Display();
+
+	void showStartUp();
+	void clear();
+	void begin();
+	void drawString(uint8_t x, uint8_t y, const char* s);
+	void showKnobsInfo(Knobs* knobs);
+	bool isTimeToUpdate();
+};
+
+#endif //_DISPLAY_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,29 @@
 #include "Arduino.h"
 #include "Knobs.h"
-#include "U8x8lib.h"
+#include "Display.h"
 
 Knobs knobs = Knobs();
+Display display = Display();
 
-// init display
-//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE); // graphic driver
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE); //text driver
 
 void setup() {
     // write your initialization code here
     Serial.begin(115200);
     Serial.println("Setup");
 
-    u8x8.begin();
-
-    u8x8.setFont(u8x8_font_amstrad_cpc_extended_r);
-    u8x8.drawString(0, 0, "Hello World!1234");
+	display.begin();
+	display.showStartUp();
+	delay(2000);
+	display.clear();
 }
 
-unsigned long t = 0;
-int displayFps = 15;
-unsigned long updateDelay = 1000 / displayFps;
-
-const int maxCharacters=16;
-char buf[maxCharacters];
-
 void loop() {
-    // write your code here
 
-    if (t < millis()) {
-        t = millis() + updateDelay;
+    // if computer connected then display knobs info
+	display.showKnobsInfo(&knobs);
 
-        for (int i = 0; i < knobs.GetNumberOfKnobs(); ++i) {
-            snprintf(buf, maxCharacters, "Knob %d: %d      ", i, knobs.GetValue(i));
-            u8x8.drawString(0, i + 1, buf);
-        }
-    }
+    // todo: wait for connection from computer
+
+
+    // todo: show receiving data
 }
